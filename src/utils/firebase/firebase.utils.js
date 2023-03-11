70388ec06daaea41ec0ebb249f6132d7b6b909ec
xuniorss <fortunatojunior2020@gmail.com>
2023-03-11 20:08:17 -0300

    aula 96 - Finish Creating User Documents

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -22,8 +22,21 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
 
 export const db = getFirestore()
 
-export const createUserDocumentFromAuth = (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth) => {
    const userDocRef = doc(db, 'users', userAuth.uid)
 
-   console.log(userDocRef)
+   const userSnapshot = await getDoc(userDocRef)
+
+   if (!userSnapshot.exists()) {
+      const { displayName, email } = userAuth
+      const createdAt = new Date()
+
+      try {
+         await setDoc(userDocRef, { displayName, email, createdAt })
+      } catch (error) {
+         console.log('error creating the user', error.message)
+      }
+   }
+
+   return userDocRef
 }
