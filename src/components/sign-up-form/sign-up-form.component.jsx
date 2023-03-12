98727c8bae39c84e505acafd3e71a115771b8ee8
xuniorss <fortunatojunior2020@gmail.com>
2023-03-11 22:38:53 -0300

    aula 100 - Sign Up With Email + Password

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
-import { createAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils'
+import {
+   createAuthUserWithEmailAndPassword,
+   createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils'
 
 const defaultFormFields = {
    displayName: '',
@@ -12,8 +15,29 @@ const SignUpForm = () => {
    const [formFields, setFormFields] = useState(defaultFormFields)
    const { displayName, email, password, confirmPassword } = formFields
 
+   const resetFormFields = () => {
+      setFormFields(defaultFormFields)
+   }
+
    const handleSubmit = async (event) => {
       event.preventDefault()
+
+      if (password !== confirmPassword) {
+         alert('passwords do not match')
+         return
+      }
+
+      try {
+         const { user } = await createAuthUserWithEmailAndPassword(email, password)
+         await createUserDocumentFromAuth(user, { displayName })
+         resetFormFields()
+      } catch (error) {
+         if (error.code === 'auth/email-already-in-use') {
+            alert('Cannot create user, email already in use')
+         } else {
+            console.log('user creation encountered an error', error)
+         }
+      }
    }
 
    const handleChange = (event) => {
@@ -24,7 +48,7 @@ const SignUpForm = () => {
    return (
       <div>
          <h1>Sign up with your email and password</h1>
-         <form onSubmit={() => {}}>
+         <form onSubmit={handleSubmit}>
             <label>Display Name</label>
             <input
                type="text"
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -29,7 +29,7 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 export const db = getFirestore()
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
    if (!userAuth) return
 
    const userDocRef = doc(db, 'users', userAuth.uid)
@@ -41,7 +41,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
       const createdAt = new Date()
 
       try {
-         await setDoc(userDocRef, { displayName, email, createdAt })
+         await setDoc(userDocRef, { displayName, email, createdAt, ...additionalInformation })
       } catch (error) {
          console.log('error creating the user', error.message)
       }
