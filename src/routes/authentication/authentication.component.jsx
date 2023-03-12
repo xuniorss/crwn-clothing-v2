ef2ed57c007d9ecb73f3fe637fb43f7407398a8d
xuniorss <fortunatojunior2020@gmail.com>
2023-03-12 12:21:31 -0300

    aula 103 - Sign In Form

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,22 +1,18 @@
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
+import SignInForm from '../../components/sign-in-form/sign-in-form.component'
 import {
    createUserDocumentFromAuth,
    signInWithGooglePopup,
 } from '../../utils/firebase/firebase.utils'
 
-const SignIn = () => {
-   const logGoogleUser = async () => {
-      const { user } = await signInWithGooglePopup()
-      const userDocRef = await createUserDocumentFromAuth(user)
-   }
-
+const Authentication = () => {
    return (
       <div>
          <h1>Sign In Page</h1>
-         <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+         <SignInForm />
          <SignUpForm />
       </div>
    )
 }
 
-export default SignIn
+export default Authentication
