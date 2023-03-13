8b7e784f87312d0a12943cf69a662c8760ade38c
xuniorss <fortunatojunior2020@gmail.com>
2023-03-12 21:48:53 -0300

    aula 121 - Cart Item Designs

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,12 +4,12 @@ import { useContext } from 'react'
 import { CartContext } from '../../contexts/cart.context'
 
 const CartIcon = () => {
-   const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
 
    return (
       <div className="cart-icon-container" onClick={() => setIsCartOpen(!isCartOpen)}>
          <ShoppingIcon className="shopping-icon" />
-         <span className="item-count">0</span>
+         <span className="item-count">{cartCount}</span>
       </div>
    )
 }
