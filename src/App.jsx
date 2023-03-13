e710076203e5ec477b30c3f9af51e7a9fbb92505
xuniorss <fortunatojunior2020@gmail.com>
2023-03-13 11:40:19 -0300

    aula 134 - Nested Routes in Shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
 
+import Authentication from './routes/authentication/authentication.component'
+import Checkout from './routes/checkout/checkout.component'
 import Home from './routes/home/home.component'
 import Navigation from './routes/navigation/navigation.component'
-import Authentication from './routes/authentication/authentication.component'
 import Shop from './routes/shop/shop.component'
-import Checkout from './routes/checkout/checkout.component'
 
 export default function App() {
    return (
       <Routes>
          <Route path="/" element={<Navigation />}>
             <Route index element={<Home />} />
-            <Route path="shop" element={<Shop />} />
+            <Route path="shop/*" element={<Shop />} />
             <Route path="auth" element={<Authentication />} />
             <Route path="checkout" element={<Checkout />} />
          </Route>
