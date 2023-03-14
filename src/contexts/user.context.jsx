1a3a51bca736f47786cbe67d1015ec4078a0def2
xuniorss <fortunatojunior2020@gmail.com>
2023-03-14 15:22:17 -0300

    aula 149 - Cart Reducer Pt. 3

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,6 +1,7 @@
-import { createContext, useEffect, useReducer, useState } from 'react'
+import { createContext, useEffect, useReducer } from 'react'
 
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from '../utils/firebase/firebase.utils'
+import { createAction } from '../utils/reducer/reducer.utils'
 
 export const UserContext = createContext({ currentUser: null, setCurrentUser: () => null })
 
@@ -28,7 +29,7 @@ export const UserProvider = ({ children }) => {
    const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE)
 
    const setCurrentUser = (user) => {
-      dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+      dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
    }
 
    const value = { currentUser, setCurrentUser }
