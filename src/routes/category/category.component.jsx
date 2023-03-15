f559785759c02c70a5792dfd2e06ace55b74de24
xuniorss <fortunatojunior2020@gmail.com>
2023-03-15 12:51:57 -0300

    aula 158 - Categories Selectors

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,13 +1,14 @@
-import { Fragment, useContext, useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
 import ProductCard from '../../components/product-card/product-card.component'
-import { CategoriesContext } from '../../contexts/categories.context'
+import { selectCategoriesMap } from '../../store/categories/category.selector'
 
-import { CategoryTitle, CategoryContainer } from './category.styles'
+import { CategoryContainer, CategoryTitle } from './category.styles'
 
 const Category = () => {
-   const { categoriesMap } = useContext(CategoriesContext)
+   const categoriesMap = useSelector(selectCategoriesMap)
    const { category } = useParams()
    const [products, setProducts] = useState(categoriesMap[category])
 
