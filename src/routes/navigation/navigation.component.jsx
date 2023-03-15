76ccb7009756971f9a764600fba740a205d90611
xuniorss <fortunatojunior2020@gmail.com>
2023-03-15 12:11:52 -0300

    aula 157 - Categories Reducer

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,7 +6,6 @@ import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import { CartContext } from '../../contexts/cart.context'
-import { UserContext } from '../../contexts/user.context'
 import { selectCurrentUser } from '../../store/user/user.selector'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 
