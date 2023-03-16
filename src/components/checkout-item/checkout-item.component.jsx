e08cba57ae60580dc2e38e2eeedd20ba96056f54
xuniorss <fortunatojunior2020@gmail.com>
2023-03-16 10:15:37 -0300

    aula 168 - Migrate Cart Context to Redux Pt. 4

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,18 @@
-import { useContext } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { CartContext } from '../../contexts/cart.context'
+import { addItemToCart, clearItemFromCart, removeItemFromCart } from '../../store/cart/cart.action'
+import { selectCartItems } from '../../store/cart/cart.selector'
 
 import './checkout-item.styles.scss'
 
 const CheckoutItem = ({ cartItem }) => {
-   const { clearItemFromCart, addItemToCart, removeItemFromCart } = useContext(CartContext)
-
    const { name, imageUrl, price, quantity } = cartItem
+   const cartItems = useSelector(selectCartItems)
+   const dispatch = useDispatch()
 
-   const clearItemFromHandler = () => clearItemFromCart(cartItem)
-   const addItemHandler = () => addItemToCart(cartItem)
-   const removeItemHandler = () => removeItemFromCart(cartItem)
+   const clearItemFromHandler = () => dispatch(clearItemFromCart(cartItems, cartItem))
+   const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem))
+   const removeItemHandler = () => dispatch(removeItemFromCart(cartItems, cartItem))
 
    return (
       <div className="checkout-item-container">
