2c2497d5d35c83977740cdf0b3b4e7e2746ca5e7
xuniorss <fortunatojunior2020@gmail.com>
2023-03-16 09:54:49 -0300

    aula 167 - Migrating Cart Context to Redux Pt. 3

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,19 @@
-import { useContext } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-// import { CartContext } from '../../assets/shopping-bag.svg'
-import { CartContext } from '../../contexts/cart.context'
+import { setIsCartOpen } from '../../store/cart/cart.action'
+import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selector'
 import { CartIconContainer, ItemCount, ShoppingIcon } from './cart-icon.styles'
 
 const CartIcon = () => {
-   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
+   const dispatch = useDispatch()
+
+   const cartCount = useSelector(selectCartCount)
+   const isCartOpen = useSelector(selectIsCartOpen)
+
+   const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen))
 
    return (
-      <CartIconContainer onClick={() => setIsCartOpen(!isCartOpen)}>
+      <CartIconContainer onClick={toggleIsCartOpen}>
          <ShoppingIcon />
          <ItemCount>{cartCount}</ItemCount>
       </CartIconContainer>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,20 +1,18 @@
-import { Fragment, useContext } from 'react'
+import { Fragment } from 'react'
 import { useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 import CartIcon from '../../components/cart-icon/cart-icon.component'
-import { CartContext } from '../../contexts/cart.context'
+import { selectIsCartOpen } from '../../store/cart/cart.selector'
 import { selectCurrentUser } from '../../store/user/user.selector'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
-
 import { LogoContainer, NavigationContainer, NavLink, NavLinks } from './navigation.styles'
 
 const Navigation = () => {
    const currentUser = useSelector(selectCurrentUser)
-
-   const { isCartOpen } = useContext(CartContext)
+   const isCartOpen = useSelector(selectIsCartOpen)
 
    return (
       <Fragment>
