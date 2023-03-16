2228a0a631b60684b6d4376668af3dd4ec672e89
xuniorss <fortunatojunior2020@gmail.com>
2023-03-15 23:28:36 -0300

    aula 166 - Migrating Cart Context to Redux Pt. 2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
-import { CartProvider } from './contexts/cart.context'
 import reportWebVitals from './reportWebVitals'
 
 import { Provider } from 'react-redux'
@@ -15,9 +14,7 @@ ReactDOM.render(
    <React.StrictMode>
       <Provider store={store}>
          <BrowserRouter>
-            <CartProvider>
-               <App />
-            </CartProvider>
+            <App />
          </BrowserRouter>
       </Provider>
    </React.StrictMode>,
--- src/store/cart/cart.selector.js
+++ src/store/cart/cart.selector.js
@@ -1,6 +1,15 @@
-const newCartCount = newCartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+import { createSelector } from 'reselect'
 
-const newCartTotal = newCartItems.reduce(
-   (total, cartItem) => total + cartItem.quantity * cartItem.price,
-   0
+const selectCartReducer = (state) => state.cart
+
+export const selectCartItems = createSelector([selectCartReducer], (cart) => cart.cartItems)
+
+export const selectIsCartOpen = createSelector([selectCartReducer], (cart) => cart.isCartOpen)
+
+export const selectCartCount = createSelector([selectCartItems], (cartItems) =>
+   cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+)
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+   cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
 )
