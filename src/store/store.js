19389af3527163c6c2a21300490540511777b19c
xuniorss <fortunatojunior2020@gmail.com>
2023-03-16 14:10:18 -0300

    aula 171 - Asynchronous Redux: Redux-Thunk

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { applyMiddleware, compose, legacy_createStore as createStore } from 'red
 import logger from 'redux-logger'
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+import thunk from 'redux-thunk'
 
 import { rootReducer } from './root-reducer'
 
@@ -13,7 +14,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean)
+const middleWares = [process.env.NODE_ENV === 'development' && logger, thunk].filter(Boolean)
 
 const composedEnhancer =
    (process.env.NODE_ENV === 'development' &&
