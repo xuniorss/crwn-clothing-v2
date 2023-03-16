7803eae79d63cf921aa29b87397c62207af7c212
xuniorss <fortunatojunior2020@gmail.com>
2023-03-15 22:53:43 -0300

    selectCurrentUserReducer

--- src/store/user/user.selector.js
+++ src/store/user/user.selector.js
@@ -1,1 +1,13 @@
-export const selectCurrentUser = (state) => state.user.currentUser
+import { createSelector } from 'reselect'
+
+const selectCurrentUserReducer = (state) => state.user
+
+export const selectCurrentUser = createSelector(
+   [selectCurrentUserReducer],
+   (currentUserSlice) => currentUserSlice.currentUser
+)
+
+// export const selectCurrentUser = (state) => {
+//    console.log('state for current user selector', state)
+//    return state.user.currentUser
+// }
