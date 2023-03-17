7a4922479e2b0d34f42ee9390de118141719229b
xuniorss <fortunatojunior2020@gmail.com>
2023-03-17 14:49:10 -0300

    aula 182 - Redux-Saga: Sign Out Sagas

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,18 +1,21 @@
 import { Fragment } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import { selectIsCartOpen } from '../../store/cart/cart.selector'
+import { signOutStart } from '../../store/user/user.action'
 import { selectCurrentUser } from '../../store/user/user.selector'
-import { signOutUser } from '../../utils/firebase/firebase.utils'
 import { LogoContainer, NavigationContainer, NavLink, NavLinks } from './navigation.styles'
 
 const Navigation = () => {
+   const dispatch = useDispatch()
    const currentUser = useSelector(selectCurrentUser)
    const isCartOpen = useSelector(selectIsCartOpen)
+
+   const signOutUser = () => dispatch(signOutStart())
 
    return (
       <Fragment>
--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -1,7 +1,7 @@
 import { all, call, put, takeLatest } from 'redux-saga/effects'
 
-import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth, getCurrentUser, signInAuthUserWithEmailAndPassword, signInWithGooglePopup } from '../../utils/firebase/firebase.utils'
-import { signInFailed, signInSucces, signUpFailed, signUpSuccess } from './user.action'
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth, getCurrentUser, signInAuthUserWithEmailAndPassword, signInWithGooglePopup, signOutUser } from '../../utils/firebase/firebase.utils'
+import { signInFailed, signInSucces, signOutFailed, signOutSuccess, signUpFailed, signUpSuccess } from './user.action'
 import { USER_ACTION_TYPES } from './user.types'
 
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
@@ -50,6 +50,15 @@ export function* signUp({ payload: { email, password, displayName } }) {
    }
 }
 
+export function* signOut() {
+   try {
+      yield call(signOutUser)
+      yield put(signOutSuccess())
+   } catch (error) {
+      yield put(signOutFailed(error))
+   }
+}
+
 export function* signInAfterSignUp({ payload: { user, additionalDetails } }) {
    yield call(getSnapshotFromUserAuth, user, additionalDetails)
 }
@@ -74,6 +83,10 @@ export function* onSignUpSuccess() {
    yield takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, signInAfterSignUp)
 }
 
+export function* onSignOutStart() {
+   yield takeLatest(USER_ACTION_TYPES.SIGN_OUT_START, signOut)
+}
+
 export function* userSagas() {
    yield all([
       call(onCheckUserSession),
@@ -81,5 +94,6 @@ export function* userSagas() {
       call(onEmailSignInStart),
       call(onSignUpStart),
       call(onSignUpSuccess),
+      call(onSignOutStart),
    ])
 }
